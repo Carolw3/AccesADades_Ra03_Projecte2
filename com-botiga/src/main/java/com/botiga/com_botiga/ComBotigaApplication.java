93ac10e46e5d4a5e31a2b114147cd64bf61cf730
Carolw3 <carolina.melguizos3@gmail.com>
2026-02-25 19:10:13 +0100

    first commit

---
+++ com-botiga/src/main/java/com/botiga/com_botiga/ComBotigaApplication.java
@@ -0,0 +1,13 @@
+package com.botiga.com_botiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBotigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBotigaApplication.class, args);
+	}
+
+}
---
+++ com-botiga/src/test/java/com/botiga/com_botiga/ComBotigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botiga.com_botiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComBotigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
